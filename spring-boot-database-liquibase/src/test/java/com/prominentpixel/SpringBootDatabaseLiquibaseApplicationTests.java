8d6945809d49ed804483c827b46b0a69e3089ac1
Akash Nar <akash.java@prominentpixel.com>
2020-07-12 11:12:29 +0530

    Spring Boot Database Migration Liquibase with Mysql

---
+++ spring-boot-database-liquibase/src/test/java/com/prominentpixel/SpringBootDatabaseLiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prominentpixel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDatabaseLiquibaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
